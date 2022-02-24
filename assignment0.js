3f0c2efbe1e4de60940640187c8a7877d073cf31
Kyle Weisse <kyle.weisse@gmail.com>
2022-02-23 19:10:48 -0500

    first commit

---
+++ assignment0.js
@@ -0,0 +1,6 @@
+let birthYear = 1983;
+const futureYear = 2088;
+let myAge1 = futureYear - birthYear;
+let myAge2 = (futureYear -1) - birthYear;
+
+console.log(`My age in ${futureYear} is either ${myAge1} or ${myAge2}`);
---
+++ practice.js
@@ -0,0 +1,8 @@
+for(var current = 0; current <20; current++){
+    if (current%2 == 0){
+        gs.print('the current number, '+ current+' is even');
+    }
+    else {
+        gs.print('the current number is odd: '+current);
+    }
+}
