26288fa86167c596870673f4e1c689269ed42b5b
Kyle Weisse <kyle.weisse@gmail.com>
2022-02-28 19:00:59 -0500

    gliderecord working somewhat

--- practice2.js
+++ practice2.js
@@ -1,7 +1,15 @@
-var someIncidents = newGlideRecord('incident')
-someIncidents.addQuery('active','true' || 'category', 'hardware')
-someInsicidents.setLimit(10)
-while (someIncidents.next()) {
-    gs.info(incidents.number + ": " + incidents.created_date + " " + incidents.short_description);
+// var someIncidents = newGlideRecord('incident')
+// someIncidents.addQuery('active','true' || 'category', 'hardware')
+// someInsicidents.setLimit(10)
+// while (someIncidents.next()) {
+//     gs.info(incidents.number + ": " + incidents.created_date + " " + incidents.short_description);
 
+// }
+
+var incidents = new GlideRecord("incident");
+incidents.addQuery("active=true^category=hardware");
+incidents.setLimit(10);
+incidents.query();
+while (incidents.next()) {
+    gs.info(incidents.number + ": " + incidents.opened_at + " " + incidents.short_description);
 }
